82263e65a4415d46e023747585b5109a2cfe1982
Olivia Carlisle <151247171+oliviacarlisle@users.noreply.github.com>
2024-05-14 16:13:50 -0400

    Initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App: React.FC = () => (
+  <div>
+    <h1>{`Hi Ocelots! We're making Cachemunk`}</h1>
+    <p>{`Here's where our amazing dashboard is going to go`}</p>
+  </div>
+);
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,63 @@
+import path from 'node:path';
+import process from 'node:process';
+import { fileURLToPath } from 'node:url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: process.env.NODE_ENV,
+  entry: './client/src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+              ['@babel/preset-react', { runtime: 'automatic' }],
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/src/index.html', // Path to your index.html
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+      publicPath: '/',
+    },
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://localhost:3000',
+      },
+    ],
+  },
+};
