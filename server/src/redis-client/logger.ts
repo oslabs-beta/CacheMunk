22272e82ba48422eb291842fae5d5611a7d85ea9
Olivia Carlisle <151247171+oliviacarlisle@users.noreply.github.com>
2024-05-16 00:16:57 -0400

    Implement simple logger function

---
+++ server/src/redis-client/logger.ts
@@ -0,0 +1,16 @@
+export const logger = async (name: string, start: bigint, end: bigint): Promise<void> => {
+  await Promise.resolve();
+
+  const nanosecondsDifference = end - start;
+  const ms = Number(nanosecondsDifference) / 1_000_000; // Convert to milliseconds
+
+  const log = {
+    level: 'info',
+    type: 'execution_time',
+    timestamp: new Date().toISOString(),
+    name,
+    execution_time: ms.toFixed(3), // execution time in milliseconds
+  };
+
+  console.log(log);
+};
