d11f56fc306872cdc2fd59e9e37d5e386803256e
jrpillai <jayanrpillai@gmail.com>
2024-05-16 16:05:53 -0500

    first prometheus commit

--- server/src/routers/dataRouter.ts
+++ server/src/routers/dataRouter.ts
@@ -1,20 +1,36 @@
 import { Router, Request, Response } from 'express';
+import { cacheHits, cacheMisses } from '../server.js'; // Import the metrics
 
 const router = Router();
 
 router.get('/cache', (req: Request, res: Response) => {
-  res.status(200).send('read with cache');
+  // Simulate cache check
+  // check cache for query key
+  const isCacheHit = Math.random() > 0.5;
+  if (isCacheHit) {
+    cacheHits.inc();
+    // if true, send back result from cache
+    res.status(200).send('Read with Cache - Cache Hit!');
+  } else {
+    cacheMisses.inc();
+    // if false, directly query the SQL database
+    // send result to the cache (use async await)
+    // send the response to the front end (use async await)
+    res.status(200).send('Read with Cache - Cache Miss!');
+  }
 });
 
 router.get('/nocache', (req: Request, res: Response) => {
   res.status(200).send('read without cache');
 });
 
-router.patch('/cache', (req: Request, res: Response) => {
+router.post('/cache', (req: Request, res: Response) => {
+  // invalidate the cache based on specified dependencies
+  // make insert query to the SQL database
   res.status(201).send('patch with cache');
 });
 
-router.patch('/nocache', (req: Request, res: Response) => {
+router.post('/nocache', (req: Request, res: Response) => {
   res.status(201).send('patch without cache');
 });
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,36 +1,98 @@
 import express, { Request, Response, NextFunction } from 'express';
-import dataRouter from './routers/dataRouter.js'
+import dataRouter from './routers/dataRouter.js';
+import client from 'prom-client';
 
 const app = express();
 
 // specify the port number to listen on
-const PORT = 3000;
+const PORT = 3030;
 
 // express middleware that parses JSON bodies
 app.use(express.json());
+
+// registry for prometheus metrics
+const register = new client.Registry();
+
+// default metrics i
+client.collectDefaultMetrics({ register });
+
+// Define metrics
+
+// Duration of HTTP Request
+const httpRequestDurationMicroseconds = new client.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method', 'route', 'status_code'],
+  buckets: [50, 100, 200, 300, 400, 500, 1000], // buckets for response time
+});
+
+// Cache Hits
+const cacheHits = new client.Counter({
+  name: 'cache_hits',
+  help: 'Number of cache hits',
+});
+
+// Cache Misses
+const cacheMisses = new client.Counter({
+  name: 'cache_misses',
+  help: 'Number of cache misses',
+});
+
+// Register the metrics
+register.registerMetric(httpRequestDurationMicroseconds);
+register.registerMetric(cacheHits);
+register.registerMetric(cacheMisses);
+
+// Middleware to collect response time
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({
+      route: req.route ? req.route.path : req.path,
+      method: req.method,
+      status_code: res.statusCode,
+    });
+  });
+  next();
+});
+
+// Export the metrics for use in other files
+export { cacheHits, cacheMisses };
 
 app.get('/ping', (req: Request, res: Response) => {
   console.log('received ping');
   res.json('pong');
 });
 
-app.use('/data', dataRouter)
+app.use('/data', dataRouter);
 
+// set metrics endpoint for prometheus
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
 
 app.use('/*', (req: Request, res: Response) => {
   res.status(404).json('Page Not Found');
 });
 
-app.use((err: { log: string, status: number, message: string}, req: Request, res: Response, next: NextFunction) => {
-  const defaultErr: object = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 500, 
-    message: { err: 'An error occurred'},
-  }
-  const errorObj = Object.assign({}, defaultErr, err)
-  return res.status(errorObj.status).json(errorObj.message)
-  // res.status(500).send(err)
-})
+app.use(
+  (
+    err: { log: string; status: number; message: string },
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    const defaultErr: object = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 500,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    return res.status(errorObj.status).json(errorObj.message);
+    // res.status(500).send(err)
+  },
+);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
