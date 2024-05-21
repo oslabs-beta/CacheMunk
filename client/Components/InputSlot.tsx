ca6b0d257ca4d756424029d3727a62e6b1184896
Nicholas Angelopoulos <leaf@Nicholass-MacBook-Pro.local>
2024-05-21 07:12:38 -0400

    more stylesheets and components

---
+++ client/Components/InputSlot.tsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+import React from "react";
+import "./style.css";
+
+interface Props {
+  className: any;
+  divClassName: any;
+  text: string;
+}
+
+export const InputSlot = ({ className, divClassName, text = "Default Slot" }: Props): JSX.Element => {
+  return (
+    <div className={`input-slot ${className}`}>
+      <div className={`default-slot ${divClassName}`}>{text}</div>
+    </div>
+  );
+};
+
+InputSlot.propTypes = {
+  text: PropTypes.string,
+};
