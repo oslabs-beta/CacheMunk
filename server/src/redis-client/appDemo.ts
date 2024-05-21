31561dfe727cf43b072ee43010381ccecdfa261e
jrpillai <jayanrpillai@gmail.com>
2024-05-20 19:00:25 -0500

    minor appDemo changes to see key-value paires

--- server/src/redis-client/appDemo.ts
+++ server/src/redis-client/appDemo.ts
@@ -5,21 +5,25 @@ const redis = new Redis({ port: 6379, host: '127.0.0.1' });
 
 // load CacheMunk
 const cache = CacheMunk(redis, {
-  onCacheHit: ((queryKey, executionTime, dataLengthBytes) => {
-    console.log(`cache hit on query ${queryKey} (${dataLengthBytes.toString()} bytes) in ${executionTime.toString()} ms. `);
-  }),
-  onCacheMiss: ((queryKey, executionTime) => {
+  onCacheHit: (queryKey, executionTime, dataLengthBytes) => {
+    console.log(
+      `cache hit on query ${queryKey} (${dataLengthBytes.toString()} bytes) in ${executionTime.toString()} ms. `,
+    );
+  },
+  onCacheMiss: (queryKey, executionTime) => {
     console.log(`cache miss on query ${queryKey} in ${executionTime.toString()} ms`);
-  }),
-  onWrite: ((queryKey, executionTime, originalSize, compressedSize) => {
-    console.log(`wrote: ${compressedSize.toString()} bytes to cache at ${queryKey} in ${executionTime.toString()} ms`);
+  },
+  onWrite: (queryKey, executionTime, originalSize, compressedSize) => {
+    console.log(
+      `wrote: ${compressedSize.toString()} bytes to cache at ${queryKey} in ${executionTime.toString()} ms`,
+    );
     console.log(`original size: ${originalSize.toString()} bytes`);
-  })
+  },
 });
 
 console.log('cachemunk loaded');
 
-const serializedResult = "someresult";
+const serializedResult = 'someresult';
 
 // set query result into the cache
 await cache.set('people:select', serializedResult, ['people']);
@@ -28,6 +32,13 @@ await cache.set('testkey', 'testvalue', []);
 // read from cache
 await cache.get('people:select');
 await cache.get('people:select2');
+
+// Read from cache and log the result
+const result1 = await cache.get('people:select');
+console.log('Result for people:select:', result1);
+
+const result2 = await cache.get('testkey');
+console.log('Result for people:select2:', result2);
 
 // when updating the cities table:
 // await cache.invalidate('people');
