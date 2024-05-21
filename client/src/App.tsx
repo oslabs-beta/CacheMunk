925f64f737fd538a1eaccf2361b64094eb3c2e6d
Nicholas Angelopoulos <leaf@Nicholass-MacBook-Pro.local>
2024-05-21 08:34:26 -0400

    more components and app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React from "react";
+import { InputSlot } from "../Components/InputSlot";
+import { Switch } from "../Components/Switch";
+import "./style.css";
 
 const App: React.FC = () => (
-  <div>
-    <h1>{`Hi Ocelots! We're making Cachemunk`}</h1>
-    <p>{`Here's where our amazing dashboard is going to go`}</p>
+  <div className="frame">
+  <div className="div">
+    <img className="image" alt="Image" src="image-1.png" />
+    <Switch className="switch-2" disabled={false} ellipseClassName="switch-instance" selected={false} />
+    <InputSlot className="input-slot-instance" divClassName="design-component-instance-node" text="Cache Switch" />
+    <img className="query-select" alt="Query select" src="query-select.png" />
+    <div className="text-wrapper">CacheMunk Demo</div>
+    <img className="donut-chart" alt="Donut chart" src="donut-chart-1.png" />
+    <div className="text-wrapper-2">Hit/Miss Ratio</div>
   </div>
+</div>
+
 );
 
 export default App;
