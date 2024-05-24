e3d13f8b051108f71a5332ea29d960b267b09d83
Olivia Carlisle <151247171+oliviacarlisle@users.noreply.github.com>
2024-05-24 12:08:21 -0400

    Remove test routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -19,49 +19,10 @@ app.get('/cache-analytics', (req, res) => {
   res.json(cacheInfo);
 });
 
-// Endpoint to get response times for /cache
+// Endpoint to get response times for /cache (number[])
 app.get('/cache-response-times', (req, res) => {
   const cacheResponseTimes = getCacheResponseTimes();
   res.json(cacheResponseTimes);
-});
-
-app.post('/test/insert', (req, res) => {
-  res.json('Endpoint: test/insert - QuerySelect: insert');
-});
-
-app.get('/test/select-cache', (req, res) => {
-  res.json('Endpoint: test/select-cache - QuerySelect: select, CacheSwitch: true');
-});
-
-app.get('/test/select-no-cache', (req, res) => {
-  res.json('Endpoint: test/select-no-cache - QuerySelect: select, CacheSwitch: false');
-});
-
-app.get('/test/costly-cache', (req, res) => {
-  res.json('Endpoint: test/costly-cache - QuerySelect: costly, CacheSwitch: true');
-});
-
-app.get('/test/costly-no-cache', (req, res) => {
-  res.json('Endpoint: test/costly-no-cache - QuerySelect: costly, CacheSwitch: false');
-});
-
-app.get('/test/initial', (req, res) => {
-  res.json('Endpoint: /test/initial - initial test');
-});
-
-// First endpoint: /test/cache-analytics
-app.get('/test/cache-analytics', (req, res) => {
-  const cacheAnalytics = {
-    cacheHits: 3,
-    cacheMisses: 4,
-  };
-  res.json(cacheAnalytics);
-});
-
-// Second endpoint: /test/cache-response-times
-app.get('/test/cache-response-times', (req, res) => {
-  const responseTimes = [13, 3, 4, 5];
-  res.json(responseTimes);
 });
 
 app.use('*', (req, res) => {
