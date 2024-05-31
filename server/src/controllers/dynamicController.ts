5dcf62ca6e02f9fa25b2cd10e52e5dba60f4fd4d
Nicholas Angelopoulos <leaf@Nicholass-MacBook-Pro.local>
2024-05-31 16:45:43 -0400

    controller for dynamic insert query

--- server/src/controllers/dynamicController.ts
+++ server/src/controllers/dynamicController.ts
@@ -3,14 +3,43 @@ import { asyncWrapper } from './errorHandling.js';
 import cache from '../cache/redisClient.js';
 import { query } from '../db.js';
 
+//Req is an object with custom name and state id - sending other values as well
+
 export const dynamicQuery = asyncWrapper(
   async (req: Request, res: Response, next: NextFunction) => {
-    //need custom insert location
+    //need custom insert name
     //need custom values to insert
-    const queryExample = `INSERT INTO table_name (column1, column2, column3, ...)
-    VALUES (value1, value2, value3, ...)`;
+    // const req.body = {
+    //   "name": "",
+    //   "state_code": "ID",
+    //   "country_id": 233,
+    //   "country_code": "US",
+    //   "latitude": "38.842460",
+    //   "longitude": "-84.021296",
+    //   "flag": true,
+    //   "state_id": 1460
+    // }
 
-    const queryText = req.body.query;
+    const { name, state_code, country_id, country_code, latitude, longitude, flag, state_id } =
+      req.body;
+
+    const queryText = `INSERT INTO cities (
+      name, state_code, country_id, country_code, latitude, longitude, flag, state_id
+    ) VALUES (
+      $1, $2, $3, $4, $5, $6, $7, $8
+    )
+    `;
+
+    const values = [
+      name,
+      state_code,
+      country_id,
+      country_code,
+      latitude,
+      longitude,
+      flag,
+      state_id,
+    ];
 
     try {
       const result = await query(queryText, values);
